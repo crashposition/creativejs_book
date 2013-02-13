c703c86ddf333eff78c34fffe885019e12783b16
John Dalziel <john@phidigital.com>
2013-02-13 21:54:19 +0000

    - updated README - removed document.body.appendChild(canvas); from embedded canvas examples

--- 01 Canvas/01 Add a canvas/03 canvas/canvas.js
+++ 01 Canvas/01 Add a canvas/03 canvas/canvas.js
@@ -3,14 +3,13 @@ var SCREEN_WIDTH = window.innerWidth,
     SCREEN_HEIGHT = window.innerHeight,
 
     // canvas element and 2D context
-    canvas = document.createElement( 'canvas' ),
-    context = canvas.getContext( '2d' );
+    canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
 
 init();
 
-function init() {
+function init(){
     // set up canvas
-    document.body.appendChild(canvas);
     canvas.width = SCREEN_WIDTH;
     canvas.height = SCREEN_HEIGHT;
 }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-creativejs_book
-===============
-
 CreativeJS Book code examples
+=============================
+
+This repo contains work in progress code examples for a CreativeJS Book.
+
+original code by Seb Lee Delisle @sebly and John Dalziel @crashposition
